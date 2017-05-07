31c6131a328daa4839c99dbf8f1c17d2caa5a74c
sangjunnn <sangjun.lee1992@gmail.com>
2017-05-08 00:52:00 +0900

    Add files via upload

---
+++ torch_install.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+echo "Torch install.."
+
+git clone "https://github.com/torch/distro.git ~/torch --recursive" "/home/sj/"
+
+cd ~/torch
+
+bash install-deps
+
+./install.sh
+
+source ~/.bashrc
+
+luarocks install image
+
+luarocks install torch
+
+luarocks install nn
+
+luarocks install lua-cjson
+
+luarocks install cutorch
+
+luarocks install cunn
+
+luarocks install cudnn
+
+luarocks install qtlua
+
+luarocks install camera
+
+luarocks lists
